aab261b797a1e692e331ef5fa7a5c913b12d9b7c
kaybrian <kayonbrian@gmail.com>
2020-06-16 13:27:13 +0300

    i have saved the first image using the files api

--- youtube/urls.py
+++ youtube/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 app_name = 'youtube'
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('upload/',views.uploads,name='upload'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- youtube/views.py
+++ youtube/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def index(request):
     return render(request,'youtube/index.html')
 
 def uploads(request):
+    if request.method == 'POST':
+        uploadedFiles = request.FILES['document']
+        fs = FileSystemStorage()
+        fs.save(uploadedFiles.name,uploadedFiles)
+    context = {}
     return render(request,'youtube/upload.html')
